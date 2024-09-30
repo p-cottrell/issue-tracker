01b6eac6a281ca9f4126f880e882611f9c78ae96
Neonalig <codychook@gmail.com>
2024-09-30 22:14:55 +0800

    Improved tiptap overflow styling

--- frontend/src/components/richtext/TiptapToolbar.js
+++ frontend/src/components/richtext/TiptapToolbar.js
@@ -1,5 +1,6 @@
 import { BoldIcon, ChatBubbleBottomCenterTextIcon, EllipsisHorizontalCircleIcon, H1Icon, H2Icon, ItalicIcon, LinkIcon, ListBulletIcon, NumberedListIcon, UnderlineIcon } from '@heroicons/react/24/outline';
 import React, { useEffect, useRef, useState } from 'react';
+import './tiptap.css';
 
 const TiptapToolbar = ({ editor }) => {
     const [showMenu, setShowMenu] = useState(false);
@@ -21,7 +22,11 @@ const TiptapToolbar = ({ editor }) => {
     const handleResize = () => {
         if (toolbarRef.current) {
             const availableWidth = toolbarRef.current.clientWidth;
-            setIsOverflow(totalButtonWidth > availableWidth);
+            const overflow = totalButtonWidth > availableWidth;
+            setIsOverflow(overflow);
+            if (!overflow) {
+                setShowMenu(false);
+            }
         }
     };
 
@@ -40,13 +45,21 @@ const TiptapToolbar = ({ editor }) => {
         return null;
     }
 
+    const getButtonGroupClass = () => {
+        return isOverflow ? 'overflow-button-group' : 'normal-button-group';
+    };
+
+    const getButtonClass = () => {
+        return isOverflow ? 'overflow-button' : 'normal-button';
+    };
+
     const buttons = (
         <>
-            <div className="btn-group" ref={(el) => (buttonsRef.current[0] = el)}>
+            <div className={`btn-group ${getButtonGroupClass()}`} ref={(el) => (buttonsRef.current[0] = el)}>
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleBold().run()}
-                    className={`btn btn-left ${editor.isActive('bold') ? 'btn-active' : ''}`}
+                    className={`btn btn-left ${getButtonClass()} ${editor.isActive('bold') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Bold"
                 >
                     <BoldIcon className="w-6 h-6" />
@@ -54,7 +67,7 @@ const TiptapToolbar = ({ editor }) => {
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleItalic().run()}
-                    className={`btn btn-middle ${editor.isActive('italic') ? 'btn-active' : ''}`}
+                    className={`btn btn-middle ${getButtonClass()} ${editor.isActive('italic') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Italic"
                 >
                     <ItalicIcon className="w-6 h-6" />
@@ -62,17 +75,17 @@ const TiptapToolbar = ({ editor }) => {
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleUnderline().run()}
-                    className={`btn btn-right ${editor.isActive('underline') ? 'btn-active' : ''}`}
+                    className={`btn btn-right ${getButtonClass()} ${editor.isActive('underline') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Underline"
                 >
                     <UnderlineIcon className="w-6 h-6" />
                 </button>
             </div>
-            <div className="btn-group">
+            <div className={`btn-group ${getButtonGroupClass()}`}>
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleBlockquote().run()}
-                    className={`btn btn-left ${editor.isActive('blockquote') ? 'btn-active' : ''}`}
+                    className={`btn btn-left ${getButtonClass()} ${editor.isActive('blockquote') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Blockquote"
                 >
                     <ChatBubbleBottomCenterTextIcon className="w-6 h-6" />
@@ -80,7 +93,7 @@ const TiptapToolbar = ({ editor }) => {
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleBulletList().run()}
-                    className={`btn btn-middle ${editor.isActive('bulletList') ? 'btn-active' : ''}`}
+                    className={`btn btn-middle ${getButtonClass()} ${editor.isActive('bulletList') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Bullet List"
                 >
                     <ListBulletIcon className="w-6 h-6" />
@@ -88,17 +101,17 @@ const TiptapToolbar = ({ editor }) => {
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleOrderedList().run()}
-                    className={`btn btn-right ${editor.isActive('orderedList') ? 'btn-active' : ''}`}
+                    className={`btn btn-right ${getButtonClass()} ${editor.isActive('orderedList') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Ordered List"
                 >
                     <NumberedListIcon className="w-6 h-6" />
                 </button>
             </div>
-            <div className="btn-group">
+            <div className={`btn-group ${getButtonGroupClass()}`}>
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-                    className={`btn btn-left ${editor.isActive('heading', { level: 1 }) ? 'btn-active' : ''}`}
+                    className={`btn btn-left ${getButtonClass()} ${editor.isActive('heading', { level: 1 }) ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Heading 1"
                 >
                     <H1Icon className="w-6 h-6" />
@@ -106,17 +119,17 @@ const TiptapToolbar = ({ editor }) => {
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-                    className={`btn btn-right ${editor.isActive('heading', { level: 2 }) ? 'btn-active' : ''}`}
+                    className={`btn btn-right ${getButtonClass()} ${editor.isActive('heading', { level: 2 }) ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Heading 2"
                 >
                     <H2Icon className="w-6 h-6" />
                 </button>
             </div>
-            <div className="btn-group">
+            <div className={`btn-group ${getButtonGroupClass()}`}>
                 <button
                     type="button"
                     onClick={() => editor.chain().focus().toggleLink({ href: prompt('Enter the URL') }).run()}
-                    className={`btn ${editor.isActive('link') ? 'btn-active' : ''}`}
+                    className={`btn ${getButtonClass()} ${editor.isActive('link') ? 'btn-active' : ''} hover:bg-gray-200 transition duration-300`}
                     title="Link"
                 >
                     <LinkIcon className="w-6 h-6" />
@@ -142,7 +155,7 @@ const TiptapToolbar = ({ editor }) => {
                 )}
             </div>
             {showMenu && (
-                <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg">
+                <div className="absolute left-0 mt-2 w-auto bg-white border border-gray-200 rounded shadow-lg z-50 flex flex-col items-center p-2">
                     {buttons}
                 </div>
             )}
